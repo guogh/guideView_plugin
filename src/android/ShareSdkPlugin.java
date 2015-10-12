30a44a0a841c240fa789d72b48106a4274fc5ddd
fuyh <fuyh@asto-inc.com>
2015-10-13 07:54:22 +0800

    updated

--- src/android/ShareSdkPlugin.java
+++ src/android/ShareSdkPlugin.java
@@ -23,7 +23,7 @@ import cn.sharesdk.wechat.moments.WechatMoments;
 public class ShareSdkPlugin extends CordovaPlugin {
     private static final String TAG = "sharesdk";
 
-    private boolean initialized = false ;
+    private String initError = null ;
 
     /**
      * Sets the context of the Command. This can then be used to do things like
@@ -35,42 +35,47 @@ public class ShareSdkPlugin extends CordovaPlugin {
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         Activity self = cordova.getActivity() ;
-        ActivityInfo info = self.getPackageManager().getActivityInfo(self.getComponentName(), PackageManager.GET_META_DATA);
-        // STEP 1: initialize the ShareSDK through code
-        ShareSDK.initSDK(cordova.getActivity(), info.applicationInfo.metaData.getString("ShareSDK_AppKey", "VPREFIX_").substring(8));
-        // STEP 2: initialize the sina web share interface
-        {
-            HashMap<String,Object> hashMap = new HashMap<String, Object>();
-            hashMap.put("Id","1");
-            hashMap.put("SortId","1");
-            hashMap.put("ShareByAppClient","true");
-            hashMap.put("Enable","true");
-            hashMap.put("AppKey", info.applicationInfo.metaData.getString("ShareSDK_SinaWeibo_AppKey", "VPREFIX_").substring(8));
-            hashMap.put("AppSecret",info.applicationInfo.metaData.getString("ShareSDK_SinaWeibo_AppSecret", "VPREFIX_").substring(8));
-            hashMap.put("RedirectUrl",info.applicationInfo.metaData.getString("ShareSDK_SinaWeibo_RedirectUrl", "VPREFIX_").substring(8));
-            ShareSDK.setPlatformDevInfo(SinaWeibo.NAME,hashMap);
-        }
-        // STEP 2: initialize the Wechat share interface
-        {
-            HashMap<String,Object> hashMap = new HashMap<String, Object>();
-            hashMap.put("Id","2");
-            hashMap.put("SortId","2");
-            hashMap.put("Enable","true");
-            hashMap.put("BypassApproval","false");
-            hashMap.put("AppId",info.applicationInfo.metaData.getString("ShareSDK_Wechat_AppId", "VPREFIX_").substring(8));
-            hashMap.put("AppSecret",info.applicationInfo.metaData.getString("ShareSDK_Wechat_AppSecret", "VPREFIX_").substring(8));
-            ShareSDK.setPlatformDevInfo(Wechat.NAME,hashMap);
-        }
-        // STEP 3: initialize the WechatMoments share interface
-        {
-            HashMap<String,Object> hashMap = new HashMap<String, Object>();
-            hashMap.put("Id","3");
-            hashMap.put("SortId","3");
-            hashMap.put("Enable","true");
-            hashMap.put("BypassApproval","false");
-            hashMap.put("AppId",info.applicationInfo.metaData.getString("ShareSDK_WechatMoments_AppId", "VPREFIX_").substring(8));
-            hashMap.put("AppSecret",info.applicationInfo.metaData.getString("ShareSDK_WechatMoments_AppSecret", "VPREFIX_").substring(8));
-            ShareSDK.setPlatformDevInfo(WechatMoments.NAME,hashMap);
+        try {
+            ActivityInfo info = self.getPackageManager().getActivityInfo(self.getComponentName(), PackageManager.GET_META_DATA);
+            // STEP 1: initialize the ShareSDK through code
+            ShareSDK.initSDK(cordova.getActivity(), info.applicationInfo.metaData.getString("ShareSDK_AppKey", "VPREFIX_").substring(8));
+            // STEP 2: initialize the sina web share interface
+            {
+                HashMap<String,Object> hashMap = new HashMap<String, Object>();
+                hashMap.put("Id","1");
+                hashMap.put("SortId","1");
+                hashMap.put("ShareByAppClient","true");
+                hashMap.put("Enable","true");
+                hashMap.put("AppKey", info.applicationInfo.metaData.getString("ShareSDK_SinaWeibo_AppKey", "VPREFIX_").substring(8));
+                hashMap.put("AppSecret",info.applicationInfo.metaData.getString("ShareSDK_SinaWeibo_AppSecret", "VPREFIX_").substring(8));
+                hashMap.put("RedirectUrl",info.applicationInfo.metaData.getString("ShareSDK_SinaWeibo_RedirectUrl", "VPREFIX_").substring(8));
+                ShareSDK.setPlatformDevInfo(SinaWeibo.NAME,hashMap);
+            }
+            // STEP 2: initialize the Wechat share interface
+            {
+                HashMap<String,Object> hashMap = new HashMap<String, Object>();
+                hashMap.put("Id","2");
+                hashMap.put("SortId","2");
+                hashMap.put("Enable","true");
+                hashMap.put("BypassApproval","false");
+                hashMap.put("AppId",info.applicationInfo.metaData.getString("ShareSDK_Wechat_AppId", "VPREFIX_").substring(8));
+                hashMap.put("AppSecret",info.applicationInfo.metaData.getString("ShareSDK_Wechat_AppSecret", "VPREFIX_").substring(8));
+                ShareSDK.setPlatformDevInfo(Wechat.NAME,hashMap);
+            }
+            // STEP 3: initialize the WechatMoments share interface
+            {
+                HashMap<String,Object> hashMap = new HashMap<String, Object>();
+                hashMap.put("Id","3");
+                hashMap.put("SortId","3");
+                hashMap.put("Enable","true");
+                hashMap.put("BypassApproval","false");
+                hashMap.put("AppId",info.applicationInfo.metaData.getString("ShareSDK_WechatMoments_AppId", "VPREFIX_").substring(8));
+                hashMap.put("AppSecret",info.applicationInfo.metaData.getString("ShareSDK_WechatMoments_AppSecret", "VPREFIX_").substring(8));
+                ShareSDK.setPlatformDevInfo(WechatMoments.NAME,hashMap);
+            }
+        } catch (Exception e) {
+            initError = e.getMessage();
+            e.printStackTrace();
         }
     }
 
@@ -98,8 +103,15 @@ public class ShareSdkPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray inputs, CallbackContext callbackContext) throws JSONException {
         try {
-            if ("share".equals(action)) {
-                share(inputs.getString(0),inputs.getString(1),inputs.getString(2),inputs.getString(3));
+            if (initError != null) {
+                callbackContext.error(initError);
+            } else if ("share".equals(action)) {
+                String title     = inputs.getJSONObject(0).getString("title");
+                String content   = inputs.getJSONObject(0).getString("content");
+                String imagePath = inputs.getJSONObject(0).getString("imagePath");
+                String url       = inputs.getJSONObject(0).getString("url");
+
+                share(title,content,imagePath,url);
                 callbackContext.success("Hello");
                 return true;
             } else {
